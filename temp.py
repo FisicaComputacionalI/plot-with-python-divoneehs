602e5f872803c346ae350a49222f6655b4a5062d
Diana Ivonee Huitzil Sosa <31254618+divoneehs@users.noreply.github.com>
2017-08-29 13:25:13 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 #Construimos el objeto plt introduciendo valores que se grafican contra el numero de entrada. Por ejemplo si queremos graficar el crecimiento de un material por dia en centimetros. 
 plt.plot([7,8,8,9,9,10,10,10,11,11,14,15,15,18,18,22,22,29,30,30])
 #Con esta isntruccion colocamos un titulo al eje Y de la grafica. 
-plt.ylabel('Num de hermanos')
+plt.ylabel('Num de hermanos y primos')
 #Con esta isntruccion guardamos la imagen con el formato que queramos.
 plt.savefig('temp.png')
 #Con esta instruccion mostramos la figura. 
